98174f5b2e1a27e07c9faddb054b55b831b97605
Jason Ho <hojasonn@users.noreply.github.com>
2016-12-09 13:10:09 -0600

    Created Mathematica package framework.

--- sumrules.m
+++ sumrules.m
@@ -1,3 +1,12 @@
 (* ::Package:: *)
 
+BeginPackage["sumrules`"]
+Unprotect @@ Names["sumrules`*"];
+ClearAll @@ Names["sumrules`*"];
 
+Begin["`Private`"]
+
+
+End[]
+Protect @@ Names["sumrules`*"];
+EndPackage[]
