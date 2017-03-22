6998dcab83a314ed89ab2c6f74a48996428b6bcc
hojasonn <jnh201@mail.usask.ca>
2017-03-22 17:51:24 -0600

    inputed quark mass data from PDGLive (2016)

--- QCDParameters.m
+++ QCDParameters.m
@@ -17,6 +17,41 @@ mc[\[Mu]]::usage="Running charm quark mass with renormalization scale \[Mu]. Def
 mb[\[Mu]]::usage="Running bottom quark mass with renormalization scale \[Mu]. Defined as ... (citation)"
 
 Begin["`Private`"]
+(*Constant Parameters*)
 
+(*Bare Light Quark Masses @2GeV- PDG 2016*)
+lightBare = 0.0035(*GeV*)(*PDG average - (u+d)/2*)
+\[delta]lightBare = {+0.0007,-0.0003}
+strangeBare = 0.096 (*GeV*)
+\[delta]strangeBare = {+0.008,-0.004}
+
+(*Bare Heavy Quark Masses @ Bare Mass - PDG 2016*)
+charmBare = 1.27 (*GeV*)
+\[delta]charmBare = {0.03,-0.03}
+bottomBare = 4.18 (*GeV*)
+\[delta]bottomBare = {0.04,-0.03}
+
+(*Quark Mass Ratios*)
+strangelightRatio = 0.0273
+\[delta]strangelightRatio  = {0.0007,-0.0007}
+charmstrangeRatio = 11.72
+\[delta]charmstrangeRatio = {0.25,-0.25}
+bottomcharmRatio = 4.53
+\[delta]bottomcharmRatio = {0.05,-0.05}
+
+(*Condensate Parameters - Sources as stated*)
+
+
+(*Running Coupling*)
+\[Alpha][\[Mu]_] = Module[
+                          {M\[Tau] = 1.77699, \[Alpha]\[Tau] = 0.330, nf = 4, b0},
+                            b0 = 1/(12 \[Pi]) (33 - 2 nf);
+                            \[Alpha]\[Tau]/(1 + b0 \[Alpha]\[Tau] Log[\[Mu]^2/M\[Tau]^2])
+                        ];
+(*Running Mass*)
+mq[\[Mu]_]:= lightBare()
+ms[\[Mu]_]:= strangeBare()
+mc[\[Mu]_]:= charmBare()
+mb[\[Mu]_]:= bottomBare()
 End[]
 EndPackage[]
