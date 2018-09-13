2d5514f2a5d24307f6403ecb4d4548f2905e5351
hojasonn <jnh201@campus.usask.ca>
2018-09-13 16:26:20 +0800

    Made arg2_ optional input; tests seem to indicate that this doesn't interfere with results.

--- applyIntegrals.m
+++ applyIntegrals.m
@@ -24,7 +24,7 @@ integralDef[p_,r_,s_,\[Alpha]1_,\[Alpha]2_,\[Alpha]3_,\[Alpha]4_]:=
 ((\[Pi]^(2+\[Epsilon]) I^(1-4-2\[Epsilon]))(p.p)^(2+\[Epsilon]-r-s))/(Gamma[r]Gamma[s]Gamma[4+2\[Epsilon]-r-s+4]) \[Nu]^(2\[Epsilon])/(\[Nu]^2)^\[Epsilon] (momentum[p,\[Alpha]1]momentum[p,\[Alpha]2]momentum[p,\[Alpha]3]momentum[p,\[Alpha]4] Gamma[2+\[Epsilon]-s]Gamma[4+2+\[Epsilon]-r]Gamma[r+s-2-\[Epsilon]]+1/2 p.p(g[\[Alpha]1,\[Alpha]2]momentum[p,\[Alpha]3]momentum[p,\[Alpha]4]+g[\[Alpha]2,\[Alpha]3]momentum[p,\[Alpha]1]momentum[p,\[Alpha]4]+g[\[Alpha]3,\[Alpha]4]momentum[p,\[Alpha]2]momentum[p,\[Alpha]1]+g[\[Alpha]1,\[Alpha]4]momentum[p,\[Alpha]2]momentum[p,\[Alpha]3])Gamma[2+\[Epsilon]+1-s]Gamma[2+\[Epsilon]+3-r]Gamma[r+s-2-\[Epsilon]-1]+1/4 (p.p)^2 (g[\[Alpha]1,\[Alpha]2]g[\[Alpha]3,\[Alpha]4]+g[\[Alpha]2,\[Alpha]3]g[\[Alpha]1,\[Alpha]4])Gamma[2+\[Epsilon]-s+2]Gamma[2+\[Epsilon]+2-r]Gamma[r+s-2-\[Epsilon]-2])
 integralDef[p_,r_,s_,\[Alpha]1_,\[Alpha]2_,\[Alpha]3_,\[Alpha]4_,\[Alpha]5_]:=(*integralDef[p,r,s,\[Alpha]1,\[Alpha]2,\[Alpha]3,\[Alpha]4,\[Alpha]5]*)(-(\[Pi]^(2+\[Epsilon]) I^(1-4-2\[Epsilon])) (p.p)^(2+\[Epsilon]-r-s))/(Gamma[r]Gamma[s]Gamma[4+2\[Epsilon]-r-s+5]) \[Nu]^(2\[Epsilon])/(\[Nu]^2)^\[Epsilon] (momentum[p,\[Alpha]1]momentum[p,\[Alpha]2]momentum[p,\[Alpha]3]momentum[p,\[Alpha]4]momentum[p,\[Alpha]5] Gamma[2+\[Epsilon]-s]Gamma[5+2+\[Epsilon]-r]Gamma[r+s-2-\[Epsilon]]+1/2 p.p(g[\[Alpha]1,\[Alpha]2]momentum[p,\[Alpha]3]momentum[p,\[Alpha]4]momentum[p,\[Alpha]5]+g[\[Alpha]2,\[Alpha]3]momentum[p,\[Alpha]1]momentum[p,\[Alpha]4]momentum[p,\[Alpha]5]+g[\[Alpha]3,\[Alpha]4]momentum[p,\[Alpha]5]momentum[p,\[Alpha]2]momentum[p,\[Alpha]1]+g[\[Alpha]4,\[Alpha]5]momentum[p,\[Alpha]1]momentum[p,\[Alpha]2]momentum[p,\[Alpha]3]+g[\[Alpha]5,\[Alpha]1]momentum[p,\[Alpha]4]momentum[p,\[Alpha]2]momentum[p,\[Alpha]3])Gamma[2+\[Epsilon]+1-s]Gamma[2+\[Epsilon]+4-r]Gamma[r+s-2-\[Epsilon]-1]+1/4 (p.p)^2 ((g[\[Alpha]1,\[Alpha]2]g[\[Alpha]3,\[Alpha]4]+g[\[Alpha]1,\[Alpha]3]g[\[Alpha]2,\[Alpha]4]+g[\[Alpha]1,\[Alpha]4]g[\[Alpha]2,\[Alpha]3])momentum[p,\[Alpha]5]+(g[\[Alpha]2,\[Alpha]3]g[\[Alpha]4,\[Alpha]5]+g[\[Alpha]2,\[Alpha]4]g[\[Alpha]3,\[Alpha]5]+g[\[Alpha]2,\[Alpha]5]g[\[Alpha]3,\[Alpha]4])momentum[p,\[Alpha]1]+(g[\[Alpha]1,\[Alpha]3]g[\[Alpha]4,\[Alpha]5]+g[\[Alpha]1,\[Alpha]4]g[\[Alpha]3,\[Alpha]5]+g[\[Alpha]1,\[Alpha]5]g[\[Alpha]3,\[Alpha]4])momentum[p,\[Alpha]2]+(g[\[Alpha]2,\[Alpha]1]g[\[Alpha]4,\[Alpha]5]+g[\[Alpha]2,\[Alpha]4]g[\[Alpha]1,\[Alpha]5]+g[\[Alpha]2,\[Alpha]5]g[\[Alpha]1,\[Alpha]4])momentum[p,\[Alpha]3]+(g[\[Alpha]2,\[Alpha]3]g[\[Alpha]1,\[Alpha]5]+g[\[Alpha]2,\[Alpha]1]g[\[Alpha]3,\[Alpha]5]+g[\[Alpha]2,\[Alpha]5]g[\[Alpha]3,\[Alpha]1])momentum[p,\[Alpha]4])Gamma[2+\[Epsilon]-s+2]Gamma[2+\[Epsilon]+2-r]Gamma[r+s-2-\[Epsilon]-2])
 
-applyIntegrals[expression_,rootarg_,arg1_,arg2_]:=Module[{denom1,numer1,denom2,numer2,exponentListDenom,exponentListNum,coeffRank,momentumList,indexList,uniqueIndexList,(*index1,index2,index3,*)rootArgVecCount,arg2VecCount,rootArgMomentum,arg2Momentum},
+applyIntegrals[expression_,rootarg_,arg1_,arg2_:Null]:=Module[{denom1,numer1,denom2,numer2,exponentListDenom,exponentListNum,coeffRank,momentumList,indexList,uniqueIndexList,(*index1,index2,index3,*)rootArgVecCount,arg2VecCount,rootArgMomentum,arg2Momentum},
 (*find any numerator and denomenator structure with the integration variable*)
 denom1=Cases[Denominator[expression],Power[Dot[arg1,arg1],_]|Power[Dot[arg1,_],_]|Power[Dot[_,arg1],_]|Power[Dot[Plus[_. arg1,_],Plus[_. arg1,_]],_]|Dot[arg1,_]|Dot[_,arg1]|Dot[Plus[_. arg1,_],Plus[_. arg1,_]](*|((-Dot[Plus[_. arg1,_],Plus[_. arg1,_]])^_)*),{0,1}];
 (*numer1=Cases[Numerator[expression]/.(a_+b_).(a_+b_)\[RuleDelayed]a.a+b.b+2a.b,Power[Dot[arg1,arg1],_]|Dot[arg1,arg1]|Power[Dot[arg1,_],_]|Dot[arg1,_]|Power[Dot[_,arg1],_]|Dot[_,arg1]|momentum[arg1,_],{0,1}];*)
@@ -40,7 +40,7 @@ coeffRank=2exponentListNum[[1]]+exponentListNum[[2]]+exponentListNum[[3]]+expone
 (*by integrating, we rip apart the dot products and leave four-vectors that match with the integralDef indices.*)
 rootArgVecCount=(exponentListNum[[2]]+exponentListNum[[3]]);
 arg2VecCount=(exponentListNum[[4]]+exponentListNum[[5]]);
-uniqueIndexList={index1,index2,index3,index4,index5, index6};
+uniqueIndexList={index1,index2,index3,index4,index5,index6};
 rootArgMomentum=If[rootArgVecCount>0,Times@@Table[momentum[rootarg,uniqueIndexList[[i]]],{i,1,rootArgVecCount}],1];
 arg2Momentum=If[arg2VecCount>0,Times@@Table[momentum[arg2,uniqueIndexList[[i]]],{i,(1+rootArgVecCount),(arg2VecCount+rootArgVecCount)}],1];
 expression/.A___ Times@@numer2/Times@@denom1/;FreeQ[{A},arg1]:>
@@ -65,7 +65,7 @@ Switch[coeffRank,
 4,integralDef[rootarg,exponentListDenom[[1]],exponentListDenom[[2]]+exponentListDenom[[3]],index1,indexList[[1]],indexList[[2]],indexList[[3]],indexList[[4]]],
 5,integralDef[rootarg,exponentListDenom[[1]],exponentListDenom[[2]]+exponentListDenom[[3]],indexList[[1]],indexList[[2]],indexList[[3]],indexList[[4]],indexList[[5]]]]
 ]
-]
+]//Expand
 applyIntegrals/: applyIntegrals[a_Plus,x_,y_,z_]:=applyIntegrals[#,x,y,z]&/@a
 
 End[]
